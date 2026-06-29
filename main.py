23016f7c3e8206007d869cf255f01d61390bf790
Bahri Sonustun <bahrison@gmail.com>
2026-06-29 16:28:44 +0300

    fix: main.py revision

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ from pycaret.classification import load_model, predict_model
 
 app = FastAPI(title="Telco Churn MLOps API")
 
+# Modeli yükle
 model = load_model('final_telco_churn_model')
+
+def create_tenure_cohort(tenure):
+    if tenure <= 12: return "High_Risk (0-1 year)"
+    elif tenure <= 24: return "Standard (1-2 year)"
+    elif tenure <= 48: return "Loyal (2-4 year)"
+    else: return "Good_Cust (4+ year)"
 
 @app.get("/")
 def read_root():
@@ -13,8 +20,11 @@ def read_root():
 
 @app.post("/predict")
 def predict_churn(data: Dict[str, Any]):
-
+   
     df_input = pd.DataFrame([data])
+    
+    if 'Tenure Months' in df_input.columns:
+        df_input['Tenure_Cohort'] = df_input['Tenure Months'].apply(create_tenure_cohort)
     
     predictions = predict_model(model, data=df_input)
     
